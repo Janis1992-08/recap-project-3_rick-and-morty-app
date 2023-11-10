89fec36777b264efc6ca76c00e31c2c1ca445b55
Janis Schiller <schillerjanis@gmail.com>
2023-11-10 11:38:02 +0100

    update character card part2

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import {CharacterCard} from "./components/CharacterCard/CharacterCard.js"
+
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
@@ -12,3 +15,17 @@ const pagination = document.querySelector('[data-js="pagination"]');
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+
+async function fetchCharacters() {
+  const response = await fetch("https://rickandmortyapi.com/api/character");
+  const data = await response.json();
+  console.log(data);
+  /* const characterName = data.results[0].name; */
+  const character = data.results[2];
+  console.log(character);
+  cardContainer.append(CharacterCard(character));
+}
+
+fetchCharacters();
+
